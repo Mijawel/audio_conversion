a59dc9ec6cef1ee660892ff24d5cf43cdff34718
Leatrice Ward <iamleatriceward@gmail.com>
2022-11-09 06:38:41 -0600

    Update

--- web/vad/index.js
+++ web/vad/index.js
@@ -1,21 +1,27 @@
 function conversionVAD(url) {
-  const ctx = new (window.AudioContext || window.webkitAudioContext)();
+  const ctx = new (window.AudioContext || window.webkitAudioContext)({
+    sampleRate: 16000,
+  });
   const gainNode = new GainNode(ctx);
 
   return new Promise(async (resolve, reject) => {
     try {
+      console.log("Transcoding audio...");
+      let count = 0;
+      const timestamps = [];
       const audio = new Audio(url);
       const audioNode = ctx.createMediaElementSource(audio);
       const myvad = await vad.AudioNodeVAD.new(ctx, {
-        onFrameProcessed: (probs) => {
-          // console.log("processed", probs);
-        },
         onSpeechStart: () => {
-          console.log("speech start");
+          const ts = ctx.getOutputTimestamp();
+          console.log("Speech start", ts.contextTime, ts.performanceTime);
+          timestamps[count] = { start: ts.contextTime, end: null };
         },
         onSpeechEnd: async (arr) => {
-          console.log("speech end");
-          resolve(arr);
+          const ts = ctx.getOutputTimestamp();
+          console.log("Speech end", ts.contextTime, ts.performanceTime);
+          timestamps[count] = { ...timestamps[count], end: ts.contextTime };
+          count++;
         },
       });
 
@@ -24,6 +30,12 @@ function conversionVAD(url) {
 
       audioNode.connect(gainNode);
       audio.play();
+
+      audio.addEventListener("ended", async () => {
+        console.log("Transcoding audio... done");
+        console.log("Timestamps", timestamps);
+        resolve(timestamps);
+      });
     } catch (error) {
       reject(error);
     }
